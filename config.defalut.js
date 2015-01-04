21264f1dcb975391c8c7dc0e421d5c639986f639
wangdong <iobee@foxmail.com>
2015-01-04 21:33:19 +0800

    add models layer

---
+++ config.defalut.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Serpurity on 1/4/2015.
+ */
+var path = require("path")
+
+var config = {
+    // mongodb 配置
+    db: 'mongodb://127.0.0.1/node_club_dev',
+    db_name: 'node_club_dev'
+}
+
+module.exports = config
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp")
+
+var jshint = require("gulp-jshint")
+
+gulp.task("lint", function () {
+    gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+})
+
+gulp.task("default", function () {
+
+})
