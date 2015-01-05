3357cebdaed057e84d12cdaa569da287b583c3d6
iobee <iobee@sina.com>
2015-01-05 10:20:05 +0800

    add proxy layer

---
+++ proxy/user.js
@@ -0,0 +1,17 @@
+var models = require("../models")
+var User = models.User
+
+/**
+ * list users
+ * - err, database error
+ * - users, user list
+ * @param {Array} names 用户名列表
+ * @param {Function} callback 回调函数
+ */
+exports.getUserList = function(names, callback) {
+    if (names.length === 0) {
+        return callback(null, [])
+    }
+
+    User.find({ loginname: { $in: name } }, callback)
+}
