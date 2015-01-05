51191b73b04cff5ba44a49065aa30f218c30c6c2
iobee <iobee@sina.com>
2015-01-05 17:54:16 +0800

    add save method

--- proxy/user.js
+++ proxy/user.js
@@ -13,5 +13,27 @@ exports.getUserList = function(names, callback) {
         return callback(null, [])
     }
 
-    User.find({ loginname: { $in: names } }, callback)
+    User.find({
+        name: {
+            $in: names
+        }
+    }, callback)
+}
+
+/**
+ * save user
+ * -err, database error
+ * -user, a new user
+ * @param {String} name 用户名
+ * @param {Function} callback
+ */
+exports.saveUser = function(name, callback) {
+    if (user) {
+        return callback(null, [])
+    }
+
+    var user = new User()
+    user.name = name
+
+    user.save(callback)
 }
--- test/proxy/user.test.js
+++ test/proxy/user.test.js
@@ -3,11 +3,22 @@ var proxy = require("../../proxy")
 var UserProxy = proxy.User
 
 describe("test/proxy/user.test.js", function() {
+    describe("#saveUser", function() {
+        it("new user", function(done) {
+            UserProxy.saveUser("nick", function(err, result) {
+                should.not.exist(err)
+                should.exist(result)
+                console.info(result)
+                done()
+            })
+        })
+    })
+
     describe("#getUserList()", function() {
         it("should show users", function(done) {
-            UserProxy.getUserList(["name"], function(err, users) {
+            UserProxy.getUserList(["nick"], function(err, users) {
                 should.not.exist(err)
-                users.should.have.length(1)
+                users.length.should.be.above(1)
                 done()
             })
         })
