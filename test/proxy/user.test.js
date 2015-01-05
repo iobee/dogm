177c228a55a598a4ece934bf3e3fc0bb8f8eeded
iobee <iobee@sina.com>
2015-01-05 16:32:23 +0800

    add test

--- test/proxy/user.test.js
+++ test/proxy/user.test.js
@@ -1,3 +1,16 @@
+var should = require("should")
 var proxy = require("../proxy")
 var UserProxy = proxy.User
 
+describe("test/proxy/user.test.js", function() {
+    describe("#getUserList()", function() {
+        it("shuold show users", functon(done) {
+            UserProxy.getUserList([name], function(err, users) {
+                should.not.exist(err)
+                user.shuuld.have.length(1)
+                done()
+            })
+        })
+    })
+
+})
